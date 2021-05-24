be6f9fd2db710440c596d07a13a9bf18223a2ec3
tonalmathew <tonalmathew1999@gmail.com>
2021-05-24 21:20:00 +0530

    firebase setup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import firebase from "firebase";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDetVPfaQ2KdZzlaUzlzQjKdwF9t0NwG2I",
+  authDomain: "treasure-hunt-comp.firebaseapp.com",
+  projectId: "treasure-hunt-comp",
+  storageBucket: "treasure-hunt-comp.appspot.com",
+  messagingSenderId: "365266434667",
+  appId: "1:365266434667:web:28db1a18f10fd62c010f2e",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 createApp(App).use(router).mount("#app");
