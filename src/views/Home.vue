5afe64f40965fe011261cad595994d52f0e6f993
tonalmathew <tonalmathew1999@gmail.com>
2021-05-25 20:40:32 +0530

    entho

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,39 @@
 <template>
   <navbar />
-  <div class="home">
+  <div class="container home py-3">
     <!-- <h1>Welcome, {{ name }}</h1> -->
     <!-- {{ name }} -->
     <!-- <img :src="name.photoURL" alt="profile" /> -->
     <!-- <button class="logout btn btn-danger" @click="Logout">Logout</button> -->
+    <div class="card mx-auto" style="width: 20rem">
+      <img class="card-img-top" :src="name.photoURL" alt="profile" />
+      <h5 class="card-title mx-auto">Level #</h5>
+      <div class="card-body">This is some text within a card body.</div>
+    </div>
+
+    <!-- <form class="form-inline">
+      <div class="form-group mx-sm-3 mb-2">
+        <input
+          type="password"
+          class="form-control"
+          id="inputPassword2"
+          placeholder="Password"
+        />
+      </div>
+      <button type="submit" class="btn btn-primary mb-2">
+        Confirm identity
+      </button>
+    </form> -->
+    <form class="p-2">
+      <div class="row">
+        <div class="col">
+          <input type="text" class="form-control" placeholder="Enter answer" />
+        </div>
+        <div class="col">
+          <input type="button" class="btn btn-primary" value="Verify" />
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -26,7 +55,7 @@ export default {
     onBeforeMount(() => {
       const user = firebase.auth().currentUser;
       if (user) {
-        name.value = user.email;
+        name.value = user;
       } else if (route.path == "/") {
         router.replace("/signup");
       }
