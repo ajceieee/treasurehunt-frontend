2e7c130c1fd187c769e4d90d7d8fb01bf4d0ce4d
tonalmathew <tonalmathew1999@gmail.com>
2021-06-07 12:48:08 +0530

    feat: updated leaderboard

--- src/views/Leaderboard.vue
+++ src/views/Leaderboard.vue
@@ -2,22 +2,43 @@
   <navbar />
   <div class="container">
     <div class="row">
-      <div class="card p-2" style="text-align: center">
-        <h1>
-          😁 Ivide entho unt 😁 <br />
-          🚧 under construction 🚧
-        </h1>
+      <div class="m-5 loading" v-if="isLoading">
+        <div class="mx-auto" style="width: 50%; text-align: center">
+          <img
+            src="../../public/minion.gif"
+            alt="Loading..."
+            width="100"
+            height="100"
+          />
+          <p>Loading...</p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import firebase from "firebase";
 import Navbar from "../components/Navbar.vue";
 
 export default {
   components: {
     Navbar,
+  },
+  data() {
+    return {
+      isLoading: true,
+    };
+  },
+  created() {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        window.location.href = "https://quaruntime.web.app";
+        console.log("Signed Out");
+      })
+      .catch((err) => alert(err.message));
   },
 };
 </script>
