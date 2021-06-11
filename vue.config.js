bc149226c532f0dc1e6f7c33929b497c5857e1a0
tonalmathew <tonalmathew1999@gmail.com>
2021-06-12 00:27:03 +0530

    feat: updated pwa

--- vue.config.js
+++ vue.config.js
@@ -6,3 +6,15 @@ module.exports = {
     public: "http://0.0.0.0:8080",
   },
 };
+
+module.exports = {
+  // ...other vue-cli plugin options...
+  pwa: {
+    themeColor: "#f2f8fa",
+    msTileColor: "#f2f8fa",
+    name: "Treasure Hunt - QuaRunTime 2.0",
+    iconPaths: {
+      msTileImage: "./public/icon.png",
+    },
+  },
+};
